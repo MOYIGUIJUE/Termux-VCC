d1e7993f69a2e047e15844d6de3155c1955c60ec
CCTV <3058084277@qq.com>
2022-09-03 15:22:55 +0800

    ARIA2C

--- Compile/Compile-bin/ARIA2C.bat
+++ Compile/Compile-bin/ARIA2C.bat
@@ -2,4 +2,4 @@
 printf 0x07 "%date:~5,5% %time:~0,-3% ["
 printf 0x03 http://aria2c.com/
 echos 0x07 ] (JSON-RPC Path) ws://127.0.0.1:6800/jsonrpc
-download.exe --conf-path="%~dp0Sourse Code\aria2.conf"
+down.exe --conf-path="%~dp0Sourse Code\aria2.conf"
